a3a80b37fe58e0429a8f88fbe3049f428e24703c
e175751@ie.u-ryukyu.ac.jp <e175751@ie.u-ryukyu.ac.jp>
2021-03-21 17:12:57 +0900

    Add env file

--- src/slackbot_settings.py
+++ src/slackbot_settings.py
@@ -1,4 +1,9 @@
-API_TOKEN = "Slack API Token"
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.environ['SLACK_BOT_API_TOKEN']
 
 DEFAULT_REPLY = "その言葉の意味を知りません"
 
